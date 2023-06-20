b024c207cb56da615835963ca292a3231c71e886
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-06-20 17:43:14 +0200

    Added mobile nav menu.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    window.addEventListener('scroll', function(e){
+        if(window.scrollY > 100){
+            this.document.querySelector('header').classList.add('is-scrolling');
+        }else{
+            this.document.querySelector('header').classList.remove('is-scrolling');
+        }
+    });
+
+    const menuBtn = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.nav-mobile');
+
+    menuBtn.addEventListener('click', function(){
+        menuBtn.classList.toggle('is-active');
+        mobileMenu.classList.toggle('is-active');
+    });
+
+
+}
